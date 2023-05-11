7576a002a4c091083dec8169b3089341734db799
NGANAMODEIJunior <116186351+NGANAMODEIJunior@users.noreply.github.com>
2023-05-11 14:20:04 +0200

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,27 +1,25 @@
 #include "Player.h"
 
-Player::Player(int y, int width, int height) : y(y) {
-	position = sf::Vector2f(0, y);
-	size = sf::Vector2f(width, height);
+Player::Player(int y, int width, int height) : position(0, y), size(width, height)
+{
 	shape.setPosition(position);
 	shape.setSize(size);
 	shape.setFillColor(sf::Color::Green);
 }
 
-Player::~Player() {}
-
-void Player::draw(sf::RenderWindow& window) {
+void Player::draw(sf::RenderWindow& window)
+{
 	position.x = sf::Mouse::getPosition(window).x - size.x / 2;
 	shape.setPosition(position);
 	window.draw(shape);
 }
 
-
-
-sf::Vector2f Player::getPosition() {
+const sf::Vector2f& Player::getPosition() const
+{
 	return position;
 }
 
-sf::Vector2f Player::getSize() {
+const sf::Vector2f& Player::getSize() const
+{
 	return size;
 }
