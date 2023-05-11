4e4255d02366ceb61c4d30c7d1a251219c764204
NGANAMODEIJunior <116186351+NGANAMODEIJunior@users.noreply.github.com>
2023-05-11 14:16:54 +0200

    Update Brick.cpp

--- Brick.cpp
+++ Brick.cpp
@@ -1,24 +1,17 @@
 #include "Brick.h"
 
 Brick::Brick(int x, int y, int width, int height, int health) :
-	position(sf::Vector2f(x, y)), size(sf::Vector2f(width, height)), health(health)
+	position(x, y), size(width, height), health(health)
 {
-	// Création de la forme rectangulaire représentant la brique
-	shape = sf::RectangleShape(size);
+	shape.setSize(size);
 	shape.setOutlineThickness(2);
 	shape.setOutlineColor(sf::Color::Black);
 	shape.setPosition(position);
 	shape.setFillColor(getColorFromLife());
 }
 
-Brick::~Brick()
-{
-	// Rien à faire dans le destructeur
-}
-
 void Brick::draw(sf::RenderWindow& window)
 {
-	// Dessine la brique si celle-ci est "en vie"
 	if (isAlive()) {
 		window.draw(shape);
 	}
@@ -26,7 +19,6 @@ void Brick::draw(sf::RenderWindow& window)
 
 sf::Color Brick::getColorFromLife()
 {
-	// Renvoie la couleur de la brique en fonction de son état de santé
 	switch (health) {
 	case 1:
 		return sf::Color::Red;
@@ -39,42 +31,32 @@ sf::Color Brick::getColorFromLife()
 	}
 }
 
-bool Brick::isAlive()
+bool Brick::isAlive() const
 {
-	// Renvoie true si health est supérieur à 0, false sinon
 	return health > 0;
 }
 
 void Brick::hit()
 {
-	// Méthode à appeler lorsque la brique est touchée
-	// Décrémente l'attribut health de 1
-	health--;
+	--health;
 	shape.setFillColor(getColorFromLife());
 }
 
-sf::Vector2f Brick::getPosition()
+const sf::Vector2f& Brick::getPosition() const
 {
-	// Accesseur de l'attribut position
 	return position;
 }
 
-sf::Vector2f Brick::getSize()
+const sf::Vector2f& Brick::getSize() const
 {
-	// Accesseur de l'attribut size
 	return size;
-}
-
-sf::RectangleShape Brick::getShape()
-{
-	// Renvoyer la forme de la brique
-	return shape;
 }
 
 void Brick::destroyAndDelete(std::deque<Brick*>& bricks)
 {
-	// Retirer la brique de la deque de briques
-	bricks.erase(std::remove(bricks.begin(), bricks.end(), this), bricks.end());
-	// Supprimer l'objet de la mémoire
+	auto it = std::find(bricks.begin(), bricks.end(), this);
+	if (it != bricks.end()) {
+		bricks.erase(it);
+	}
 	delete this;
 }
