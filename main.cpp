151a47e1721f4544e2ad209d59e3e30ecd76a28c
NGANAMODEIJunior <116186351+NGANAMODEIJunior@users.noreply.github.com>
2023-05-12 10:59:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,37 +34,30 @@ int main(int argc, char** argv)
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "ma magnifique fenetre");
 
-	/*sf::RectangleShape rectangle;
-	rectangle.setSize(sf::Vector2f(100, 50));
-	rectangle.setFillColor(sf::Color::Cyan);
-	rectangle.setOutlineColor(sf::Color::Magenta);
-	rectangle.setOutlineThickness(2);
-	rectangle.setPosition(100, 100);
-	sf::CircleShape circle;
-	circle.setRadius(10);
-	circle.setPosition(200, 170);
-	circle.setFillColor(sf::Color::Red);
-	circle.setOutlineColor(sf::Color::Yellow);
-	circle.setOutlineThickness(2);*/
-
 	sf::RectangleShape rdr2;
 	rdr2.setSize(sf::Vector2f(window.getSize().x, window.getSize().y));
 
 	sf::Clock clock;
 	sf::Vector3f xFactor(10, 20, 30);
 	float ellapsedTime = 0;
+	
+	
+	    // Création d'une instance de l'objet avec lequel vous voulez vérifier la collision
+		sf::RectangleShape object;
+		object.setPosition(100, 100);
+		object.setSize(sf::Vector2f(50, 50));
 
 
-	// on fait tourner le programme jusqu'� ce que la fen�tre soit ferm�e
+	   // on fait tourner le programme jusqu'a ce que la fen�tre soit ferm�e
 	while (window.isOpen())
 	{
 		ellapsedTime = clock.restart().asSeconds();
 
-		// on inspecte tous les �v�nements de la fen�tre qui ont �t� �mis depuis la pr�c�dente it�ration
+		// on inspecte tous les evenements de la fen�tre qui ont �t� �mis depuis la pr�c�dente it�ration
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			// �v�nement "fermeture demand�e" : on ferme la fen�tre
+			// evenement "fermeture demandee" : on ferme la fenetre
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
@@ -84,10 +77,6 @@ int main(int argc, char** argv)
 			window.draw(rdr2);
 
 
-			/*double mappedValue = mapValue(i, 0, window.getSize().y, 0, 255);
-			rdr2.setFillColor(sf::Color(mappedValue, mappedValue * xFactor.x * ellapsedTime, mappedValue * xFactor.y * ellapsedTime, mappedValue * xFactore.z * ellapsedTime));
-			rdr2.setPosition(0, i);
-			window.draw(rdr2);*/
 		}
 
 
@@ -109,6 +98,12 @@ int main(int argc, char** argv)
 			}
 		}
 		window.display();
+
+		// Vérification de la collision
+		sf::FloatRect objectBounds = object.getGlobalBounds();
+		if (player.checkCollision(objectBounds)) {
+			// Collision détectée entre le joueur et l'objet.
+		}
 	}
 
 	return 0;
